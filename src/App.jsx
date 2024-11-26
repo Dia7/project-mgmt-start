5ab32255538eaecd2ba2c68f800571f362120648
Diana <diana.ratcheva@gmail.com>
2024-11-26 15:46:58 +0000

    Add button component

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <main className="h-screen my-8 flex gap-8">
       <Sidebar />
       <NoProjectSelected />
-      <NewProject/>
+      {/* <NewProject/> */}
     </main>
   );
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,13 +1,13 @@
 import React from "react";
+import Button from "./Button.jsx";
 
 export default function Sidebar() {
   return (
     <aside className=" px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
       <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">Your projects</h2>
       <div>
-      <button className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100">+ Add new project</button>
+        <Button>+ Add new project</Button>
       </div>
-      
     </aside>
   );
 }
