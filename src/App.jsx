6c6d290b108200748599e738b8616409d0b4bd51
Diana <diana.ratcheva@gmail.com>
2024-11-26 12:57:44 +0000

    Add input element

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Sidebar from "./components/Sidebar";
 import NoProjectSelected from "./components/NoProjectSelected";
+import NewProject from "./components/NewProject";
 
 function App() {
   return (
     <main className="h-screen my-8 flex gap-8">
       <Sidebar />
       <NoProjectSelected />
+      <NewProject/>
     </main>
   );
 }
--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,3 +1,19 @@
+import Input from "./Input";
+
 export default function NewProject() {
-  return <section></section>;
+  return <div>
+    <menu>
+      <li>
+        <button>Cancel</button>
+      </li>
+      <li>
+        <button>Save</button>
+      </li>
+    </menu>
+    <div>
+     <Input label="Title"/>
+     <Input label="Description" textarea />
+     <Input label="Due Date"/>
+    </div>
+  </div>;
 }
