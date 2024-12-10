b93958db73ae5fb8d5e9d794ec5d8e33cb1adb52
Diana <diana.ratcheva@gmail.com>
2024-12-10 08:25:50 +0000

    add project title in sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,54 @@
-import React from "react";
-import Sidebar from "./components/Sidebar";
-import NoProjectSelected from "./components/NoProjectSelected";
-import NewProject from "./components/NewProject";
+import { useState } from 'react';
+
+import Sidebar from "./components/Sidebar.jsx";
+import NoProjectSelected from "./components/NoProjectSelected.jsx";
+import NewProject from "./components/NewProject.jsx";
 
 function App() {
+  const [projectState, setProjectState] = useState( {
+    selectedProjectId: undefined,
+    projects: []
+  });
+
+  function handleStartAddProject() {
+    setProjectState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: null
+      }
+    });  
+  }
+
+  function handleAddProject(projectData) {
+    setProjectState((prevState) => {
+      const projectId = Math.random();
+      const newProject = {
+        ...projectData,
+        id: projectId
+      }
+      
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: [...projectState.projects, newProject]
+      }
+    })
+
+  }
+
+  let content;
+
+  if(projectState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />
+  } else if(projectState.selectedProjectId === null) {
+    content = <NewProject onAdd={handleAddProject} />
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <Sidebar />
-      <NoProjectSelected />
-      {/* <NewProject/> */}
+      <Sidebar onStartAddProject={handleStartAddProject}
+       projects={projectState.projects} />
+      {content}
     </main>
   );
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,13 +1,21 @@
-import React from "react";
+
+
 import Button from "./Button.jsx";
 
-export default function Sidebar() {
+export default function Sidebar({onStartAddProject, projects}) {
   return (
     <aside className=" px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
       <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">Your projects</h2>
       <div>
-        <Button>+ Add new project</Button>
+        <Button onClick={onStartAddProject}>+ Add new project</Button>
       </div>
+      <ul>
+        {projects.map((project) => (
+          <li key={project.id}>
+            <button className="w-full text-left px-2 py-1 rounded-sm my-1 text-stone-400 hover:text-stone-200">{project.title}</button>
+          </li>
+        ))}
+      </ul>
     </aside>
   );
 }
